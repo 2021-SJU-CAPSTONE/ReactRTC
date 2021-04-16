014791ce7042264e1af64c0eac2e636b8802582a
BeADia <koc0819@gmail.com>
2021-04-16 19:09:39 +0900

    temp save

--- src/index.tsx
+++ src/index.tsx
@@ -1,10 +1,10 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import App from "./App";
+import WebRTC from "./View/WebRTC";
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <WebRTC />
   </React.StrictMode>,
   document.getElementById("root")
 );
