85bedec508e4e3f78aec94aa9ba1d78e93c94e87
BeADia <koc0819@gmail.com>
2021-05-04 19:31:08 +0900

    fix roomController

--- src/controller/roomController.ts
+++ src/controller/roomController.ts
@@ -4,7 +4,7 @@ export const createRoom = async () => {
   const response = firestore.collection("Test");
 
   const data = await response.get();
-  data.docs.forEach(item => {
+  data.docs.forEach((item) => {
     console.log(item.data());
   });
 };
